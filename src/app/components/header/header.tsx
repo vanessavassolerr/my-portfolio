617c8c3d79940b13cebba3b1b6882669452c907b
Vanessa Vassoler <vvassoler10@gmail.com>
2023-06-11 23:29:17 -0300

    feat: add html structure and header

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import "./header.scss"
+
+
+
+export function Header(){
+    return(
+        <div className='header'>
+        <div>
+          <h1>Hi, I'm Vanessa!</h1>
+          <h2>Software Engineer</h2>
+        </div>
+        <Image
+          src="/profile.png"
+          alt="Vercel Logo"
+          width={250}
+          height={250}
+          priority
+        />
+        </div>
+    )
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,36 @@
-import Image from 'next/image'
+import { Header } from './components/header/header'
+import "./styles/home.scss"
 
 export default function Home() {
   return (
-    <main>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={100}
-        height={24}
-        priority
-      />
+    <main className='container'>
+      <Header/>
+      <div className='experience'>
+        <h3>Experience</h3>
+        <p>One year working as a Software Engineer Intern in Itau Unibanco</p>
+        <div className='experience-time'>
+
+        </div>
+
+        <div className='infos'>
+          <h3>Languages</h3>
+        <div className="languages-info">
+          <span>🇺🇸 EN - Advanced</span>
+          <span>🇧🇷 PT-BR - Native Speaker</span>
+        </div>
+        <h3>Education</h3>
+        <div className="educational-info">
+          <span> </span>
+          <span>Telecommunications Engineering - State University of Campinas</span>
+        </div>
+        </div>
+        <div className="buttons">
+          <div className="social">
+
+          </div>
+          <button>Contact me</button>
+        </div>
+        </div>
     </main>
   )
 }
